8f2921bd9b81cc49ca25ba9a8673219cf4f2825a
Kal Aeolian <kal.aeolian@nwea.org>
2021-12-16 21:49:20 -0800

    sigh bash

--- setup/spamassassin.sh
+++ setup/spamassassin.sh
@@ -47,11 +47,11 @@ echo "public.pyzor.org:24441" > /etc/mail/spamassassin/pyzor/serversdn
 #   is Spamassassin (spamc)'s own default. Specified in KBytes.
 # * Disable localmode so Pyzor, DKIM and DNS checks can be used.
 
-cat > /etc/sysconfig/spampd << EOF;
+cat > /etc/sysconfig/spampd << EOF
 SPAMPD_OPTIONS="-a -L --maxsize 500 --host 127.0.0.1:10026 --relayhost 127.0.0.1:10027"
 EOF
 
-cat > /etc/systemd/system/multi-user.target.wants/spampd.service << EOF;
+cat > /etc/systemd/system/multi-user.target.wants/spampd.service << 'EOF'
 [Unit]
 Description=spampd daemon
 After=syslog.target network.target
