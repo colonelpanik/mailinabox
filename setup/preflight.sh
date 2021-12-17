574ae716f5d2127f72a93c8001b5e8710f68ba28
Kal Aeolian <kal.aeolian@nwea.org>
2021-12-16 16:59:53 -0800

    Make centos8 instead of ubuntu check

--- setup/preflight.sh
+++ setup/preflight.sh
@@ -8,12 +8,8 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Check that we are running on Ubuntu 18.04 LTS (or 18.04.xx).
-if [ "$(lsb_release -d | sed 's/.*:\s*//' | sed 's/18\.04\.[0-9]/18.04/' )" != "Ubuntu 18.04 LTS" ]; then
-	echo "Mail-in-a-Box only supports being installed on Ubuntu 18.04, sorry. You are running:"
-	echo
-	lsb_release -d | sed 's/.*:\s*//'
-	echo
-	echo "We can't write scripts that run on every possible setup, sorry."
+if [ "$(grep -q 'Linux release 8')" -eq 0 ]
+        "Just centos 8 for now"
 	exit 1
 fi
 
