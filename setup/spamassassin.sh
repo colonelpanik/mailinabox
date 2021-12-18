75cdec38ca8e751aed9b978bcb413196b9d3c566
Kal Aeolian <kal.aeolian@nwea.org>
2021-12-17 18:03:16 -0800

    add spampd user

--- setup/spamassassin.sh
+++ setup/spamassassin.sh
@@ -166,6 +166,7 @@ tools/editconf.py /etc/mail/spamassassin/local.cf -s \
 	bayes_path=$STORAGE_ROOT/mail/spamassassin/bayes \
 	bayes_file_mode=0666
 
+useradd -r spampd -s /sbin/nologin
 mkdir -p $STORAGE_ROOT/mail/spamassassin
 chown -R spampd:spampd $STORAGE_ROOT/mail/spamassassin
 
