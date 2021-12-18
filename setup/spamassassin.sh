a242b7520b680ff2d02c80fea01400e0aa92046f
Kal Aeolian <kal.aeolian@nwea.org>
2021-12-17 20:17:10 -0800

    negate it

--- setup/spamassassin.sh
+++ setup/spamassassin.sh
@@ -69,7 +69,7 @@ ExecStart=/usr/sbin/spampd --nodetach -u spampd -g spampd --homedir /var/spool/s
 WantedBy=multi-user.target
 EOF
 # Add user if not existing
-if ! id spampd &>/dev/null ; then
+if id spampd &>/dev/null ; then
     useradd -r -s /bin/login spampd
 fi
 
