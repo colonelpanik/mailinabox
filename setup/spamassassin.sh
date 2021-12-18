3c9f7185a4b6730222376727d363a547fe7b061b
Kal Aeolian <kal.aeolian@nwea.org>
2021-12-17 18:28:08 -0800

    make it quiet

--- setup/spamassassin.sh
+++ setup/spamassassin.sh
@@ -69,7 +69,7 @@ ExecStart=/usr/sbin/spampd --nodetach -u spampd -g spampd --homedir /var/spool/s
 WantedBy=multi-user.target
 EOF
 # Add user if not existing
-if ! grep spampd /etc/passwd ; then
+if ! grep -q spampd /etc/passwd ; then
     useradd -r -s /bin/login spampd
 fi
 
